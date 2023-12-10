210293902edec71c28b41ae46ad33d0505fb94ea
heroleggo <h3r0.y0u@gmail.com>
2023-12-10 23:29:20 +0900

    init

---
+++ src/main/kotlin/com/example/alcoholjarangserver/AlcoholJarangServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.alcoholjarangserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlcoholJarangServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlcoholJarangServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/alcoholjarangserver/AlcoholJarangServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.alcoholjarangserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlcoholJarangServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
